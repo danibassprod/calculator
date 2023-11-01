35277ba638987a74c3827a93dd698537664bd9c0
Daniel <cristianobajista21@gmail.com>
2023-11-01 00:55:33 -0400

    Make the calculator work with basic operations

--- calc.js
+++ calc.js
@@ -16,14 +16,54 @@ function divide(a, b) {
 
 let numA;
 let numB;
-let operator;
+let operator = '';
+let displayNumb = '';
 
-function operation(numA, numB, operator) {
-    if (operator === '+') return sum(numA, numB)
-    if (operator === '-') return subtract(numA,numB)
-    if (operator === '*') return multiply(numA, numB)
-    if (operator === '/') return divide(numA, numB)
+function operation(a, b, op) {
+    if (operator === '') return 'ERROR'
+    if (operator === '+') return sum(a, b)
+    if (operator === '-') return subtract(a,b)
+    if (operator === '*') return multiply(a, b)
+    if (operator === '/') return divide(a, b)
 
     operator !== '+' || operator !== '-' || 
     operator !== '*' || operator !== '/' ? console.log('Not an operator') : null
 }
+
+const results = document.querySelector('.result')
+
+const acBtn = document.querySelector('.ac')
+acBtn.addEventListener('click', function(){
+    displayNumb = ''
+    results.textContent = ''
+})
+
+const delBtn = document.querySelector('.del')
+delBtn.addEventListener('click', function(){
+    let arr = displayNumb.split('')
+    arr.pop()
+    displayNumb = arr.join('')
+    results.textContent = displayNumb;
+})
+
+const operators = document.querySelectorAll('.op')
+operators.forEach(op => op.addEventListener('click', function(){
+    console.log(this.textContent)
+    operator = this.textContent;
+}))
+
+const equals = document.querySelector('.equals')
+equals.addEventListener('click', function(){
+    let arr = displayNumb.split(operator)
+    numA = parseInt(arr[0])
+    numB = parseInt(arr[1])
+    displayNumb = operation(numA, numB, operator)
+    displayNumb = displayNumb.toString()
+    results.textContent = displayNumb;
+})
+
+const num = document.querySelectorAll('#result')
+num.forEach(x => x.addEventListener('click', function(){
+    displayNumb += this.textContent;
+    results.textContent = displayNumb;
+}))
