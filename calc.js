63be8bcb93624abc55c7e6e341465b19e6ef7f6d
Daniel <cristianobajista21@gmail.com>
2023-11-06 19:31:07 -0400

    Replace calc logic

--- calc.js
+++ calc.js
@@ -16,51 +16,44 @@ function divide(a, b) {
 
 let numA;
 let numB;
-let operator = '';
+let operator = ''
+let operation = '';
 let displayNumb = '';
 
-function operation(a, b, op) {
-    if (operator === '') return 'ERROR'
-    if (operator === '+') return sum(a, b)
-    if (operator === '-') return subtract(a,b)
-    if (operator === '*') return multiply(a, b)
-    if (operator === '/') return divide(a, b)
+function operate(a, b, op) {
+    if (op === '') return 'ERROR'
+    if (op === '+') return sum(a, b)
+    if (op === '-') return subtract(a, b)
+    if (op === '*') return multiply(a, b)
+    if (op === '/') return divide(a, b)
 }
 
-const results = document.querySelector('.result')
+const display = document.querySelector('.result');
+display.textContent = displayNumb;
 
-const acBtn = document.querySelector('.ac')
-acBtn.addEventListener('click', function(){
-    displayNumb = ''
-    results.textContent = ''
-})
-
-const delBtn = document.querySelector('.del')
-delBtn.addEventListener('click', function(){
-    let arr = displayNumb.split('')
-    arr.pop()
-    displayNumb = arr.join('')
-    results.textContent = displayNumb;
-})
+const numbers = document.querySelectorAll('#number');
+numbers.forEach(num => num.addEventListener('click', function(){
+    operation += this.textContent;
+    displayNumb += this.textContent; 
+    display.textContent = displayNumb;
+}))
 
 const operators = document.querySelectorAll('.op')
 operators.forEach(op => op.addEventListener('click', function(){
-    console.log(this.textContent)
+    if (operator !== '') getResult()
     operator = this.textContent;
+    operation += this.textContent;
+    displayNumb = '';
 }))
 
 const equals = document.querySelector('.equals')
-equals.addEventListener('click', function(){
-    let arr = displayNumb.split(operator)
+equals.addEventListener('click', getResult)
+
+function getResult(){
+    let arr = operation.split(operator)
     numA = +arr[0]
     numB = +arr[1]
-    displayNumb = operation(numA, numB, operator)
-    displayNumb = displayNumb.toString()
-    results.textContent = displayNumb;
-})
-
-const num = document.querySelectorAll('#result')
-num.forEach(x => x.addEventListener('click', function(){
-    displayNumb += this.textContent;
-    results.textContent = displayNumb;
-}))
+    let finalResult = operate(numA, numB, operator)
+    operation = finalResult;
+    display.textContent = finalResult.toString()
+}
